062f6b7fc09d35575492ceea600e393dba35179e
mannumishra <mannumishra2207@outlook.com>
2024-05-08 11:31:24 +0530

    add change

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,29 +7,29 @@ const Footer = () => {
                 <div className="footer">
                     <div className="footermain">
                         <div className="first">
-                            <p>SANJIVAN ANUSHANDHAN</p>
-                            <p>Your one-stop shop for authentic Ayurvedic medicines and products, delivering the ancient wisdom of India's natural healing tradition to your doorstep.</p>
+                            <p className='footerheading'>SANJIVAN ANUSHANDHAN</p>
+                            <p className='fotertext'>Your one-stop shop for authentic Ayurvedic medicines and products, delivering the ancient wisdom of India's natural healing tradition to your doorstep.</p>
                         </div>
                         <div className="second">
-                            <p> CONTACT US:</p>
-                            <p>Address:
+                            <p className='footerheading'> CONTACT US:</p>
+                            <p className='fotertext'>Address:
                                 Sanjivan Anusandhan Pvt. Ltd.
                                 Plot No. 1880, Rai Industrial Area, Rai, Haryana 131029</p>
-                            <p>Call Us: 9810000523 / 9810020523</p>
-                            <p>Email:
+                            <p className='fotertext'>Call Us: 9810000523 / 9810020523</p>
+                            <p className='fotertext'>Email:
                                 sanjivan.anusandhan@gmail.com
                                 sanjivan.anusandhan1880@gmail.com</p>
                         </div>
                         <div className="third">
-                            <p>LEGAL INFORMATION</p>
-                            <p>Privacy Policy</p>
-                            <p>Refund Policy</p>
-                            <p>Terms & conditions</p>
-                            <p>Shipping & Delivery Policy</p>
+                            <p className='footerheading'>LEGAL INFORMATION</p>
+                            <p className='fotertext'>Privacy Policy</p>
+                            <p className='fotertext'>Refund Policy</p>
+                            <p className='fotertext'>Terms & conditions</p>
+                            <p className='fotertext'>Shipping & Delivery Policy</p>
                         </div>
                         <div className="four">
-                            <p>FOLLOW US</p>
-                            <p>Follow and discover the time-tested secrets of Ayurveda with us!</p>
+                            <p className='footerheading'>FOLLOW US</p>
+                            <p className='fotertext'>Follow and discover the time-tested secrets of Ayurveda with us!</p>
                             <div className="iconset">
                                 <i class="ri-facebook-circle-fill"></i>
                                 <i class="ri-instagram-line"></i>
