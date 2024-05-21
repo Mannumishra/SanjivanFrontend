b04fb1c89a711fbf3d08757e3fafc920763db813
mannumishra <mannumishra2207@outlook.com>
2024-05-21 14:52:38 +0530

    addchnages

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,6 @@
 import React, { useEffect } from 'react'
 import './About.css'
-import banare1 from '../Image/banare1.jpg'
-import banare2 from '../Image/banare2.jpg'
-import banare3 from '../Image/banare3.jpg'
-import banare4 from '../Image/banare4.jpg'
+import aboutbanre from '../Image/Sanjivan Anusandhan banner 6.jpg'
 import aboutimg from '../Image/about.jpg'
 import team from '../Image/team1.webp'
 import team1 from '../Image/team2.webp'
@@ -21,36 +18,7 @@ const About = () => {
         <>
             <section className='mainhome'>
                 <div className="home">
-                    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-                        <div class="carousel-indicators">
-                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 3"></button>
-                        </div>
-                        <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <img src={banare1} class="d-block w-100" alt="..." />
-                            </div>
-                            <div class="carousel-item">
-                                <img src={banare2} class="d-block w-100" alt="..." />
-                            </div>
-                            <div class="carousel-item">
-                                <img src={banare3} class="d-block w-100" alt="..." />
-                            </div>
-                            <div class="carousel-item">
-                                <img src={banare4} class="d-block w-100" alt="..." />
-                            </div>
-                        </div>
-                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Previous</span>
-                        </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Next</span>
-                        </button>
-                    </div>
+                   <img src={aboutbanre} alt="" style={{width:"100%"}}/>
                 </div>
             </section>
             <section>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -14,74 +14,27 @@ const Navbar = () => {
     const deActiveMobMode = () => {
         setIsMobModeActive(false)
     }
-
-    // const activeSearchBar = () => {
-    //     setIsSearchActive(!isSearchActive)
-    // }
-
-    // const deActiveSearchBar = () => {
-    //     setIsSearchActive(false)
-    // }
     return (
         <>
             <header className='header-section'>
                 <div className="header-container">
-                    {/* <div className="top-header">
-                    <span>Spread your payments with KLARNA</span>
-                </div> */}
-                    {/* <div className="mid-header">
-                    <div className="left">
-                        <a href="">Tel: 012345689</a>
-                        <Link to={''} >Showroom</Link>
-                        <Link to={'/contact'} >Contact</Link>
-                    </div>
-                    <div className="right">
-                        <i class="ri-instagram-line"></i>
-                        <i class="ri-facebook-line"></i>
-                        <i class="ri-pinterest-line"></i>
-                    </div>
-                </div> */}
                     <div className="bottom-header">
                         <div className="top">
-                            {/* <div className="search">
-                            <div className="icon">
-                                <i class="ri-search-line" ></i>
-                                <i class="ri-menu-line" onClick={activeMobMode}></i>
-                            </div> */}
-                            {/* <div className={`search-box ${isSearchActive ? 'active-searchbar' : ''}`} >
-                                <input type="text" placeholder="Search" />
-                                <button>Search</button>
-                            </div> */}
-                            {/* <div className={`search-box ${isSearchActive ? 'active-searchbar' : ''}`} >
-                                <input type="text" placeholder="Search" />
-                                <button>Search</button>
-                            </div> */}
-                            {/* </div> */}
                             <div className={`logo ${isSearchActive ? 'logo-none' : ''}`} >
                                 <Link to="/"> <img src={logo} alt="" className='logiimg' /></Link>
-                                {/* <h2>50<span>Tola</span></h2> */}
                             </div>
-                            {/* <div className="bag">
-                            <i class="ri-user-line"></i>
-                            <i class="ri-shopping-bag-line"></i>
-                        </div> */}
                             <nav className="nav">
                                 <ul>
                                     <li><Link to="/">Home</Link></li>
                                     <li><Link to="/about">About</Link></li>
                                     <li><Link to="/product">Our Categories</Link></li>
                                     <li><Link to='/cirtificates' >Our Certifications</Link></li>
-                                    {/* <li><Link>Careers</Link></li> */}
                                     <li><Link to="/contact">CONTACT FOR MANUFACTURING</Link></li>
-
-                                    {/* <li><Link>JOURNAL</Link></li>
-                            <li><Link to="/about">ABOUT</Link></li>
-                            <li><Link to={'/contact'}>CONTACT</Link></li> */}
                                 </ul>
                             </nav>
                             <div className="search">
                                 <div className="icon">
-                                    <i class="ri-search-line" style={{display:"none"}}></i>
+                                    <i class="ri-search-line" style={{ display: "none" }}></i>
                                     <i class="ri-menu-line" onClick={activeMobMode}></i>
                                 </div>
                                 <div className={`search-box ${isSearchActive ? 'active-searchbar' : ''}`} >
@@ -105,24 +58,8 @@ const Navbar = () => {
                             <li><Link to="/about" onClick={deActiveMobMode}>About</Link></li>
                             <li><Link to="/contact" onClick={deActiveMobMode}>CONTACT FOR MANUFACTURING</Link></li>
                             <li><Link to="/product" onClick={deActiveMobMode}>Our Categories</Link></li>
-                            {/* <li><Link onClick={deActiveMobMode}>Careers</Link></li> */}
                             <li><Link to="/cirtificates" onClick={deActiveMobMode}>Our Certifications</Link></li>
-                            {/* <li><Link onClick={deActiveMobMode}>JOURNAL</Link></li>
-                        <li><Link onClick={deActiveMobMode}>ABOUT</Link></li>
-                        <li><Link onClick={deActiveMobMode}>CONTACT</Link></li>
-                        <li><Link onClick={deActiveMobMode}>MY ACCOUNT</Link></li> */}
                         </ul>
-                        {/* <div className="social-link">
-                            <div onClick={deActiveMobMode} className="link-icon">
-                                <i class="ri-instagram-line"></i>
-                            </div>
-                            <div onClick={deActiveMobMode} className="link-icon">
-                                <i class="ri-facebook-line"></i>
-                            </div>
-                            <div onClick={deActiveMobMode} className="link-icon">
-                                <i class="ri-pinterest-line"></i>
-                            </div>
-                        </div> */}
                     </div>
                 </div>
             </header>
